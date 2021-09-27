43f4ac6579252a8609d99fffef8f435bee417467
abhishekdatta12 <81951956+abhishekdatta12@users.noreply.github.com>
2021-09-28 00:39:42 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,255 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Internshala</title>
+	<link rel="stylesheet" href="styles.css">
+	<script src="https://code.iconify.design/2/2.0.4/iconify.min.js"></script>
+</head>
+<body>
+	<div class="nav-1">
+		<img src="logo.jpg" height= 80vw width=125vw/>
+		<div class="nav-options">
+			<div class="nav-item">Internships</div>
+			<div class="nav-item">Online Trainings</div>
+			<div class="nav-item">Fresher Jobs</div>
+			<button class="nav-btn1">Login</button>
+			<button class="nav-btn2">Register</button>
+		</div>
+	</div>
+	<hr/>
+	<div class="search">
+		<input type="text" class="search-bar" placeholder="What are you looking for?" />
+		<div class="search-logo"><span class="iconify" data-icon="eva:search-fill" style="color: white;"></span></div>
+	</div>
+	<br>
+	<br>
+	<div class="internships">
+		<div class="internships-left">
+			<h1>Internships</h1><br>
+			<h3>Apply to 10,000+ internships</h3>
+		</div>
+		<div class="internships-right">
+			<h3>View all internships -></h3>
+		</div>
+	</div>
+	<div class="popular">
+		<h3>Popular Cities</h3>
+		<br/><br/>
+		<div class="cards">
+			<div class="card">
+				<img src="images/wfh.svg"/><br/>
+				<h4>Work From Home</h4>
+			</div>
+			<div class="card">
+				<img src="images/delhi.svg"/><br/>
+				<h4>Delhi/NCR</h4>
+			</div>
+			<div class="card">
+				<img src="images/bangalore.svg"/><br/>
+				<h4>Bangalore</h4>
+			</div>
+			<div class="card">
+				<img src="images/mumbai.svg"/><br/>
+				<h4>Mumbai</h4>
+			</div>
+			<div class="card">
+				<img src="images/hyderabad.svg"/><br/>
+				<h4>Hyderabad</h4>
+			</div>
+			<div class="card">
+				<img src="images/chennai.svg"/><br/>
+				<h4>Chennai</h4>
+			</div>
+			<div class="card">
+				<img src="images/kolkata.svg"/><br/>
+				<h4>Kolkata</h4>
+			</div>
+			<div class="card">
+				<img src="images/international.svg"/><br/>
+				<h4>International</h4>
+			</div>
+		</div>
+	</div>
+
+	<div class="popular">
+		<h3>Popular Categories</h3>
+		<br/><br/>
+		<div class="cards">
+			<div class="card">
+				<img src="images/part_time.svg"/><br/>
+				<h4>Part-time</h4>
+			</div>
+			<div class="card">
+				<img src="images/engineering.svg"/><br/>
+				<h4>Engineering</h4>
+			</div>
+			<div class="card">
+				<img src="images/ngo.svg"/><br/>
+				<h4>NGO</h4>
+			</div>
+			<div class="card">
+				<img src="images/mba.svg"/><br/>
+				<h4>MBA</h4>
+			</div>
+			<div class="card">
+				<img src="images/design.svg"/><br/>
+				<h4>Design</h4>
+			</div>
+			<div class="card">
+				<img src="images/science.svg"/><br/>
+				<h4>Science</h4>
+			</div>
+			<div class="card">
+				<img src="images/media.svg"/><br/>
+				<h4>Media</h4>
+			</div>
+			<div class="card">
+				<img src="images/humanities.svg"/><br/>
+				<h4>humanities</h4>
+			</div>
+		</div>
+	</div>
+	<br>
+	<br>
+	<div class="internships">
+		<div class="internships-left">
+			<h1>Internshala Trainings</h1><br>
+			<h3>Learn new-age skills on the go</h3>
+		</div>
+		<div class="internships-right">
+			<h3>View all trainings -></h3>
+		</div>
+	</div>
+	<div class="popular">
+	<div class="cards">
+		<div class="card">
+			<img src="images/programming_with_python.svg"/><br/>
+			<h4>Programming with Python</h4>
+		</div>
+		<div class="card">
+			<img src="images/digital_marketing.svg"/><br/>
+			<h4>Digital Marketing</h4>
+		</div>
+		<div class="card">
+			<img src="images/web_development.svg"/><br/>
+			<h4>Web Development</h4>
+		</div>
+		<div class="card">
+			<img src="images/machine_learning.svg"/><br/>
+			<h4>Machine Learning</h4>
+		</div>
+		<div class="card">
+			<img src="images/advanced_excel.svg"/><br/>
+			<h4>Advanced Excel</h4>
+		</div>
+		<div class="card">
+			<img src="images/ethical_hacking.svg"/><br/>
+			<h4>Ethical Hacking</h4>
+		</div>
+		<div class="card">
+			<img src="images/autocad.svg"/><br/>
+			<h4>Autocad</h4>
+		</div>
+		<div class="card">
+			<img src="images/creative_writing.svg"/><br/>
+			<h4>Creative Writing</h4>
+		</div>
+	</div>
+	<br/><br/>
+	<div class="fifty-two">& 52 more..</div>
+	
+	</div>
+	<br/><br/><br/>
+	<div class="internships">
+		<div class="internships-left">
+			<h1>Fresher Jobs</h1><br>
+			<h3>Premium fresher jobs on your fingertips</h3>
+		</div>
+		<div class="internships-right">
+			<h3>View all jobs -></h3>
+		</div>
+	</div>
+	<div class="fresher">
+	<div class="cards">
+		<div class="fresher-card">
+			<img src="images/ctc.svg"/><br/>
+			<h4>Minimum CTC of 3LPA</h4>
+		</div>
+		<div class="fresher-card">
+			<img src="images/dream.svg"/><br/>
+			<h4>Dream companies</h4>
+		</div>
+		<div class="fresher-card">
+			<img src="images/verified.svg"/><br/>
+			<h4>100% verified jobs</h4>
+		</div>
+	</div></div>
+	<br/><br/><br/><br/>
+	<footer>
+		<div class="lists">
+			<ul>
+				<li class="footer-heading">Internships by places</li>
+				<li>Internship in India</li>
+				<li>Internship in Delhi</li>
+				<li>Internship in Bangalore</li>
+				<li>Internship in Hyderabad</li>
+				<li>Internship in Mumbai</li>
+				<li>Internship in Chennai</li>
+				<li>Internship in Gurgaon</li>
+				<li>Internship in Kolkata</li>
+				<li>Virtual internship</li>
+			</ul>
+			<ul>
+				<li class="footer-heading">Internship by Stream</li>
+				<li>Computer Science Internship</li>
+				<li>Electronics Internship</li>
+				<li>Mechanical Internship</li>
+				<li>Civil Internship</li>
+				<li>Marketing Internship</li>
+				<li>Chemical Internship</li>
+				<li>Finance Internship</li>
+				<li>Summer Research Fellowship</li>
+				<li>Campus Ambassador Program</li>
+			</ul>
+			<ul>
+				<li class="footer-heading">Online Trainings</li>
+				<li>Programming with Python</li>
+				<li>Digital Marketing</li>
+				<li>Web Development</li>
+				<li>Machine Learning</li>
+				<li>Advanced Excel</li>
+				<li>Ethical Hacking</li>
+				<li>AutoCAD</li>
+				<li>Creative Writing</li>
+				<li>Data Science</li>
+			</ul>
+			<ul>
+				<li class="footer-heading">About Internshala</li>
+				<li>About us</li>
+				<li>We're hiring</li>
+				<li>Hire interns for your company</li>
+				<li>Team Diary</li>
+				<li>Blog</li>
+				<li>Our Services</li>
+				<li>Terms & Conditions</li>
+				<li>Privacy</li>
+				<li>Contact us</li>
+			</ul>
+		</div>
+		<br/>
+		<hr/>
+		<br/>
+		<div class="contact">
+			<div class="contact-options">
+				<button class="android-app"><span class="iconify" data-icon="teenyicons:google-play-store-outline" style="color: white;"></span> &nbsp;Get Android App</button>
+				<p class="contact-icon"><span class="iconify" data-icon="akar-icons:instagram-fill" style="color: white;"></span></p>
+				<p class="contact-icon"><span class="iconify" data-icon="akar-icons:twitter-fill" style="color: white;"></span></p>
+				<p class="contact-icon"><span class="iconify" data-icon="ant-design:youtube-filled" style="color: white;"></span></p>
+				<p class="contact-icon"><span class="iconify" data-icon="eva:linkedin-outline" style="color: white;"></span></p>
+			</div>
+			<p> &copy; Copyright 2021 Internshala</p>
+		</div>
+	</footer>
+</body>
+</html>
